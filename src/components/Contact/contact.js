c4900be874fd0b5d83272256055f116dd54df27b
Souvik48 <iamsouvikdas48@gmail.com>
2020-06-21 12:00:47 +0530

    Added a contact page

---
+++ src/components/Contact/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Heading from "../Reusuable/Heading"
+
+
+export default function Contact() {
+    return (
+        <section className="py-3">
+            <Heading title="Contact Us"/>
+            
+        </section>
+    )
+}
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,31 +5,26 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import HeroSection from "../components/Reusuable/HeroSection"
 import InfoBlock from "../components/Reusuable/InfoBlock"
-import DualInfoBlock from "../components/Reusuable/DualInfoBlock"
-import Teamphotosection from "../components/About/Teamphotosection"
+import Contact from "../components/Contact/contact"
 
 
-const AboutPage = ({data}) => (
+const ContactPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
     <HeroSection 
     
     img= {data.img.childImageSharp.fluid}
-    title="About LearnCodeOnline" //go to css
+    title="Contact-us"
     subtitle=""
     heroclass="about-background" //go to css to style this hero-background
-    /> 
-    <DualInfoBlock heading="A message from CEO" 
-    img= "https://images.pexels.com/photos/1261427/pexels-photo-1261427.jpeg?cs=srgb&dl=person-holding-node-text-1261427.jpg&fm=jpg"
     />
-    <InfoBlock heading="About Vision" />
-    <Teamphotosection />
+    <InfoBlock heading="How can we help?" />
   </Layout>
 )
 //<InfoBlock heading="About Us" />
 export const query = graphql`
 {
-  img: file(relativePath: { eq: "about.png" }) {
+  img: file(relativePath: { eq: "contact.png" }) {
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid_tracedSVG
@@ -40,5 +35,5 @@ export const query = graphql`
 `
 
 
-export default AboutPage
+export default ContactPage
  
